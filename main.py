6a00f9522623805de4064b1a012c7937b32f9d59
manith-dell <manithhetti@gmail.com>
2025-01-30 14:22:02 +0000

    display blank screen

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ from settings import *
 
 class PlasticPatrol:
     def __init__(self):
-        logger.info("About to initialize a pygame")
+
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption("Plastic Patrol")
-        logger.info("pygame initialized")
+        settings = Settings()
+        screen = pygame.display.set_mode((settings.SCREEN_WIDTH,settings.SCREEN_HEIGHT))
+
+        clock = pygame.time.Clock()
+
+        while True:
+        # Process player inputs.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    raise SystemExit
+
+    # Do logical updates here.
+    # ...
+
+            screen.fill("blue")  # Fill the display with a solid color
+
+    # Render the graphics here.
+    # ...
+
+            pygame.display.flip()  # Refresh on-screen display
+            clock.tick(60)
 
 
+if __name__ == '__main__':
+    game = PlasticPatrol()
